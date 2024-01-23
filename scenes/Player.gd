a8ccc88a71ba9475f7a2e29030c204b183328232
John-Macao <52253296+John-Macao@users.noreply.github.com>
2024-01-22 22:06:49 -0500

    Movement and animation player

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,29 +1,38 @@
 extends CharacterBody2D
 
-#####  TESTTTTTT
+var speed : int
+var screen_size : Vector2
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _ready():
+	screen_size = get_viewport_rect().size
+	position = screen_size / 2;
+	speed=200;
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+func get_input():
+	#keyboard input
+	var input_dir = Input.get_vector("left","right","up","down")
+	velocity = input_dir.normalized() * speed;
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	#player movement
+	get_input()
 	move_and_slide()
+	#limit  movement  to window size
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	#player animation
+	if velocity.length() != 0:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.frame = 1;
+	
+	#player rotation
+	var mouse = get_local_mouse_position();
+	var angle = snappedf(mouse.angle(), PI/4 ) / (PI/4);
+	angle = wrapi(int(angle), 0, 8);
+	
+	$AnimatedSprite2D.animation = "walk" + str(angle);
+
+
+
