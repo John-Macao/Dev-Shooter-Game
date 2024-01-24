92c8a4fb9e5342c0eb682bdcfcc98c8e7944b2af
John-Macao <52253296+John-Macao@users.noreply.github.com>
2024-01-23 23:52:12 -0500

    Added enemy AI
    Added enemy AI to hit the player

--- scenes/enemy_spawner.gd
+++ scenes/enemy_spawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var main = get_node("/root/Main");
+signal hit_p;
 
 var goblin_scene := preload("res://scenes/goblin.tscn")
 var spawn_points := [] 
@@ -12,12 +13,21 @@ func _ready():
 			spawn_points.append(i)
 	
 func _on_timer_timeout():
-	#pick random spawn point
-	var spawn  = spawn_points[randi() % spawn_points.size()]
+	#check how many enemies have already been created
+	var enemies = get_tree().get_nodes_in_group("enemies")
 	
-	#spawn enemy
-	var goblin = goblin_scene.instantiate()
-	goblin.position = spawn.position
-	main.add_child(goblin)
+	if enemies.size() < get_parent().max_enemies:
+		#pick random spawn point
+		var spawn  = spawn_points[randi() % spawn_points.size()]
+		#spawn enemy
+		var goblin = goblin_scene.instantiate()
+		goblin.position = spawn.position
+		goblin.hit_player.connect(hit)
+		main.add_child(goblin)
+		goblin.add_to_group("enemies")
+	
+func hit():
+	hit_p.emit()
+	
 	
 	
---
+++ scenes/main.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var aux:int =0 #count enemies hit
+
+var max_enemies:int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	max_enemies=10;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_enemy_spawner_hit_p():
+	aux=aux+1
+	print("Enemy hit player: " + str(aux))
